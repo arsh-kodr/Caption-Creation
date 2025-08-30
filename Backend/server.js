8352bf7e7f2fee05e6218ec5c2bb9c45f62d131b
arsh-kodr <raisahab2364@gmail.com>
2025-08-30 16:54:22 +0530

    Update app.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,9 @@
 require("dotenv").config();
-const path = require("path");
 const express = require("express");
 const connectToDB = require("./src/db/db");
 const app = require("./src/app");
 
 
-app.use(express.static(path.join(__dirname, "public")));
-
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 
 
 const PORT = process.env.PORT || 8080;
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,34 +3,21 @@ const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
 const postRoutes = require("./routes/post.routes");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 
 
-const allowedOrigins = [
-  "http://localhost:5173",  // Vite local frontend
-  "http://localhost:8080",  // Backend local
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps, curl, Postman)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // allow cookies/auth headers
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
+
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+});
 
 module.exports = app;
