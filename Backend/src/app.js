ff059727f745605d8d6339b329a08ac1a96c0e5b
arsh-kodr <raisahab2364@gmail.com>
2025-08-30 16:59:04 +0530

    fixing deployment bugs

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,10 +7,10 @@ const path = require("path");
 
 const app = express();
 
-
 app.use(cors());
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
