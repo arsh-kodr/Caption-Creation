547f630d312bf165398c76e761b7d7bb10bc3c55
arsh-kodr <raisahab2364@gmail.com>
2025-08-30 17:02:15 +0530

    last commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,11 +6,9 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
